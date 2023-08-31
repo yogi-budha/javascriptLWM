2e2950ee5a4733024d155dce9e4139354ec5c83e
yogi-budha <bhimbudha959@gmail.com>
2023-08-31 07:24:26 +0545

    add the files

---
+++ script.js
@@ -0,0 +1,9 @@
+var first=document.getElementById("#input-first")
+
+var second = document.getElementById("input-second");
+
+var operator=document.getElementById("#operator");
+
+var result =document.getElementById("result");
+
+second.innerHTML="123"
